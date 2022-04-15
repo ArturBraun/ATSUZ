d3ca98b69aaf8ae4e11a3550f6a39a5f35b4dddd
ArturBraun <55237940+ArturBraun@users.noreply.github.com>
2022-04-15 21:23:37 +0200

    Added page-not-found

--- src/main/frontend/src/components/page-not-found/Page-not-found.js
+++ src/main/frontend/src/components/page-not-found/Page-not-found.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { getOrigin } from '../../common/Common-functions'
 
 const PageNotFound = (props) => {
   return (
-    <div>
-        <p>Ups, wyszukiwana strona nie została odnaleziona lub nastąpił nieoczekiwany błąd.</p>
+    <div class="row d-flex aligns-items-center justify-content-center">
+        <div class="col-md-12 col-sm-12">
+            <div class="card shadow-lg rounded-lg mt-5 mx-auto not-found-element" style={{width: "30rem"}}>
+                <h3 class="card-header display-1 text-muted text-center">
+                    Ups...
+                </h3>
+
+                <div class="card-subtitle mt-3 mb-1 text-muted text-center">
+                  Wyszukiwana strona nie została odnaleziona lub nastąpił nieoczekiwany błąd. 
+                </div>
+
+                <div class="card-body mx-auto">
+                    <a type="button" href={getOrigin()} class="btn btn-sm btn-success text-white"> Powrót do strony głównej </a>
+                </div>
+            </div>
+        </div>
     </div>
   )
 };
