09f08ddc370badc890858d1be01bfba0ec2be64b
ArturBraun <55237940+ArturBraun@users.noreply.github.com>
2022-04-16 11:10:46 +0200

    Added removing from cart

--- src/main/frontend/src/common/Common-functions.js
+++ src/main/frontend/src/common/Common-functions.js
@@ -59,6 +59,32 @@ export function getCartElementById(productIdParam){
     return products[idx];
 }
 
+export function deleteFromShoppingCart(productIdParam){
+    const CART_ITEM = "SHOPPING_CART"   
+
+    let cartContent = localStorage.getItem(CART_ITEM);
+    let products = JSON.parse(cartContent)  
+    let idx = -1;
+    for(let i = 0; i < products.length; i++){
+        if(products[i].productId === productIdParam){
+            idx = i;
+            break;
+        }
+    }
+    if(idx < 0) return;
+
+    if(products[idx].amount > 1){
+        products[idx].amount = products[idx].amount - 1;
+    }
+    else{
+        products.splice(idx, 1);
+    }
+
+    const productsStr = JSON.stringify(products)
+    localStorage.setItem(CART_ITEM, productsStr)
+    console.log(`Usunieto jeden produkt z id=${productIdParam}. Zawartosc koszyka to ${productsStr}`) 
+}
+
 export function redirectToErrorPage(){
     window.location.href = `${window.location.origin.toString()}/not-found`
 }
--- src/main/frontend/src/components/shopping-cart/Shopping-cart.js
+++ src/main/frontend/src/components/shopping-cart/Shopping-cart.js
@@ -3,20 +3,39 @@ import { useParams } from "react-router-dom";
 import ProductList from '../product-list/Product-list';
 import { useEffect, useState } from 'react';
 import { sendPostRequest, sendGetRequest } from '../../services/ApiCall'
-import { getOrigin, getShoppingCartContent, getCartElementById } from '../../common/Common-functions'
+import { 
+    getOrigin, 
+    getShoppingCartContent, 
+    getCartElementById, 
+    addToShoppingCart, 
+    deleteFromShoppingCart 
+} from '../../common/Common-functions'
+import Image from '../image/Image';
+import loading from '../icons/loading.svg'
 
 const ShoppingCart = (props) => {
     const params = useParams();
     const [products, setProducts] = useState('')
+    const [cartEvent, setCartEvent] = useState(false)
 
     const calculateCartPrice = () => {
         const cartContent = getShoppingCartContent();
         let sum = 0;
         products.map(product => {
             const cartElem = getCartElementById(product.id);
-            sum = sum + cartElem.amount * product.price;
+            if(cartElem) sum = sum + cartElem.amount * product.price;
         })
         return sum;
+    }
+
+    const addToCart = (productId) => {
+        addToShoppingCart(productId)
+        setCartEvent(!cartEvent)
+    }
+
+    const deleteFromCart = (productId) => {
+        deleteFromShoppingCart(productId)
+        setCartEvent(!cartEvent)
     }
 
     useEffect( () => {    
@@ -29,7 +48,7 @@ const ShoppingCart = (props) => {
         }        
       }
       fetchData()
-    }, [])
+    }, [cartEvent])
 
     return (
         <div>
@@ -40,7 +59,7 @@ const ShoppingCart = (props) => {
                             <div className="form-inline d-flex justify-content-center">
                                 <h4 className="form-boarder form-control mb-3">
                                     {
-                                        products ? (
+                                        products.length > 0 ? (
                                             "Twój koszyk"
                                         ) : (
                                             "Twój koszyk jest pusty."
@@ -48,7 +67,7 @@ const ShoppingCart = (props) => {
                                     }
                                 </h4>
                                 {
-                                    products ? (<></>) : (
+                                    products.length > 0 ? (<></>) : (
                                         <a type="button" href={getOrigin()} 
                                             className="form-boarder form-control mb-3 btn btn-success text-white"> 
                                             Powrót do strony głównej
@@ -56,7 +75,7 @@ const ShoppingCart = (props) => {
                                     )
                                 }  
                                 {
-                                    products ? (
+                                    products.length > 0 ? (
                                         <h4 className="form-boarder form-control mb-3">
                                             Sumaryczna wartość koszyka:
                                             <strong>
@@ -71,8 +90,46 @@ const ShoppingCart = (props) => {
                 </div>
             </div>
             {
-                products ? (
-                    <ProductList data={products} />
+                products.length > 0 ? (
+                    <div className="container mt-2 mb-5">
+                        <div className="d-flex justify-content-center row">
+                            <div className="col-md-10">
+                                {
+                                    products.map(product => (
+
+                                        <div className="row mt-1 mb-2 p-2 bg-light border rounded" key={product.id}>
+                                            <a className="col-md-3 mt-1" href={`/product/${product.id}`}>
+                                                <Image imageLink={product.imageId ? `/api/v1/image?id=${product.imageId}` : loading} 
+                                                    altText={`Zdjecie produktu ${product.name}`} />
+                                            </a>
+                                            <div className="align-items-center align-content-center col-md-6 mt-2 ">
+                                                <a className="product-link" href={`/product/${product.id}`}>
+                                                    <h5>{product.name}</h5>
+                                                </a>
+                                                <div className="pt-3">
+                                                    <p className="text-muted">Ilość dni oczekiwania na dostawę: {product.deliveryWaitingTime}</p>
+                                                    <p className="text-truncate">{product.description}</p>
+                                                </div>
+                                            </div>
+                                            <div className="align-items-center align-content-center col-md-3 border-left mt-1 text-center">
+                                                <h4 className="m-2">
+                                                    {getCartElementById(product.id) ? getCartElementById(product.id).amount : 0} x {product.price.toFixed(2)} zł
+                                                </h4>
+                                                <div className="d-flex flex-column mt-4">
+                                                    <button className="btn btn-success btn-sm" type="button" onClick={() => addToCart(product.id)}>
+                                                        Dodaj kolejny
+                                                    </button>
+                                                    <button className="btn btn-outline-success btn-sm mt-2" type="button" onClick={() => deleteFromCart(product.id)}>
+                                                        Usuń jeden
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }                                            
+                            </div>
+                        </div>
+                    </div>
                 ) : (<></>)
             } 
         </div>
