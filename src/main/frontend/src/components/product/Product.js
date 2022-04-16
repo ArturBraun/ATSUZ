012a3ebee0190aa78b493707ab773542815f21b7
ArturBraun <55237940+ArturBraun@users.noreply.github.com>
2022-04-16 11:17:43 +0200

    Added adding featured product to chart

--- src/main/frontend/src/components/product/Product.js
+++ src/main/frontend/src/components/product/Product.js
@@ -3,9 +3,17 @@ import { useEffect, useState } from 'react';
 import { sendGetRequest } from '../../services/ApiCall'
 import loading from '../icons/loading.svg'
 import './Product.css'
+import { 
+    addToShoppingCart, 
+} from '../../common/Common-functions'
+import QuickMessage from '../quick-message/Quick-message';
 
 const Product = (props) => {
     const [productData, setProductData] = useState('')
+
+    const addToCart = (productId) => {
+        addToShoppingCart(productId)
+    }
 
     useEffect( () => {    
         const fetchData = async () => {    
@@ -23,28 +31,31 @@ const Product = (props) => {
       }, [props])
 
     return (
-        <div className="card product-card center-element-content"> 
-            <a href={`/product/${props.id}`}>
-                <Image imageLink={productData.imageId ? `/api/v1/image?id=${productData.imageId}` : loading} altText={`Zdjecie produktu ${productData.name}`} />
-            </a>
-            
-            <div className="card-body">
-                <a className="card-title product-link" href={`/product/${props.id}`}>
-                    <h5>{productData.name}</h5>
+        <>
+            <div className="card product-card center-element-content"> 
+                <a href={`/product/${props.id}`}>
+                    <Image imageLink={productData.imageId ? `/api/v1/image?id=${productData.imageId}` : loading} altText={`Zdjecie produktu ${productData.name}`} />
                 </a>
                 
-                <p>{productData.price ? productData.price.toFixed(2) : ' '} zł</p>
-                <button className="btn btn-outline-success add-to-cart-btn" type="submit" title="Dodaj do koszyka">
-                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3 6H22L19 16H6L3 6ZM3 6L2.25 3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                        <path d="M9.99219 11H11.9922M13.9922 11H11.9922M11.9922 11V9M11.9922 11V13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                        <path d="M11 19.5C11 20.3284 10.3284 21 9.5 21C8.67157 21 8 20.3284 8 19.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                        <path d="M17 19.5C17 20.3284 16.3284 21 15.5 21C14.6716 21 14 20.3284 14 19.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                </button>
-            </div>
-        </div>      
-
+                <div className="card-body">
+                    <a className="card-title product-link" href={`/product/${props.id}`}>
+                        <h5>{productData.name}</h5>
+                    </a>
+                    
+                    <p>{productData.price ? productData.price.toFixed(2) : ' '} zł</p>
+                    <button className="btn btn-outline-success add-to-cart-btn" title="Dodaj do koszyka" type="button" onClick={() => addToCart(productData.id)}
+                        data-bs-toggle="modal" data-bs-target="#add-to-cart-modal">
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 6H22L19 16H6L3 6ZM3 6L2.25 3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M9.99219 11H11.9922M13.9922 11H11.9922M11.9922 11V9M11.9922 11V13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M11 19.5C11 20.3284 10.3284 21 9.5 21C8.67157 21 8 20.3284 8 19.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M17 19.5C17 20.3284 16.3284 21 15.5 21C14.6716 21 14 20.3284 14 19.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                    </button> 
+                </div>
+            </div> 
+            <QuickMessage id="add-to-cart-modal"/>            
+        </>
     )
 }
 
